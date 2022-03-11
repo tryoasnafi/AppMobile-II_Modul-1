8052fcf39a96de57e3a242605338a33f5d046ad7
Tryo Asnafi <tryoasnafi@gmail.com>
2022-03-11 16:45:25 +0700

    Percobaan blok dan komentar

---
+++ src/main/kotlin/6_BlockAndComment.kt
@@ -0,0 +1,15 @@
+// Block adalah subprogram berisi statement-statement yang dikelompokkan menjadi satu bagian di dalam {}
+fun main() {
+    // Komentar satu baris
+    /*
+       Ini komentar
+       multi baris
+     */
+    /**
+     * @description deskripsi dari sebuah function,class etc
+     * @param {string} name, parameter name dalam function
+     * @return {string} nilai kembalian dari function
+     * @see https://kotlinlang.org/
+     * @see <a href="https://googlechrome.github.io/samples/classes-es6/">Dokumentasi Chrome ES6</a> untuk keterangan lebih lanjut
+     */
+}
