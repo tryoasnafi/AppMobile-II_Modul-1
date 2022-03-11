d2b2de0516d37c34e630e0c6727ab31ad226e533
Tryo Asnafi <tryoasnafi@gmail.com>
2022-03-11 16:28:02 +0700

    Percobaan Keyword

---
+++ src/main/kotlin/3_Keyword.kt
@@ -0,0 +1,10 @@
+fun main() {
+    // Keyword merupakan kata-kata khusus yang telah dipesan oleh bahasa pemrograman
+    // sehingga kita tidak boleh menggunakan kata tersebut sebagai nama variabel/fungsi/kelas etc.
+    try {} catch (e: Exception) {}
+    var variableName = "Value can be change"
+    val variableImmutable = "Constant Value"
+
+    // Menghasilkan error karena kita menggunakan keyword break sebagai variable name
+    var break = ""
+}
