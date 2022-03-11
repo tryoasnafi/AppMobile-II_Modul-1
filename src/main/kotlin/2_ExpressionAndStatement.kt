fc04739d6a6a87eb85bfa6d24788707dae2b80f2
Tryo Asnafi <tryoasnafi@gmail.com>
2022-03-11 16:19:25 +0700

    Percobaan Expression dan Statement

---
+++ src/main/kotlin/2_ExpressionAndStatement.kt
@@ -0,0 +1,9 @@
+fun main() {
+    // Expression adalah sintaks yang menghasilkan nilai
+    Integer.sum(20, 103)
+    2 * 5 + 10 - 9 / 3
+
+    // Statement adalah sintaks yang melakukan sesuatu (aksi)
+    val x = 20 % 4 // mendeklarasi dan menginisialisasi variabel x
+    println(x) // menampilkan nilai variabel x ke layar
+}
