dd98271c64e96980037538502058860460f19c58
Tryo Asnafi <tryoasnafi@gmail.com>
2022-03-11 16:31:43 +0700

    Percobaan Whitespace

---
+++ src/main/kotlin/3_Whitespaces.kt
@@ -0,0 +1,6 @@
+fun main() {
+    // Whitespace merupakan karakter yang hanya terdiri dari spasi, tab, baris baru
+    println(                )
+    print("    ")
+    println("Sebuah teks berisi whitespace")
+}
