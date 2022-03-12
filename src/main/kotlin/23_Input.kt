247a18239221310c17c7ef96387e76b2062fd4a8
Tryo Asnafi <tryoasnafi@gmail.com>
2022-03-13 00:18:09 +0700

    update: Penjelasan Percobaan Menangani Masukan (Input)

--- src/main/kotlin/23_Input.kt
+++ src/main/kotlin/23_Input.kt
@@ -1,12 +1,13 @@
 fun main() {
+    // readline() berguna untuk membaca ketikan keyboard
     print("Siapa nama kamu? ")
-    val inputName: String = readLine()!!
+    val inputName: String = readLine()!! // !! digunakan untuk memberi tahu bahwa tidak boleh null atau harus di-input
 
     print("Berapa umur kamu? ")
-    val inputAge: Int = readLine()!!.toInt()
+    val inputAge: Int = readLine()!!.toInt() // konversi menjadi Integer
 
     print("Apakah kamu pelajar? (true/false): ")
-    val isStudent: Boolean = readLine()!!.toBoolean()
+    val isStudent: Boolean = readLine()!!.toBoolean() // konversi menjadi boolean
 
     println("$inputName, $inputAge tahun, ${if (isStudent) "pelajar" else "bukan pelajar"}")
 
