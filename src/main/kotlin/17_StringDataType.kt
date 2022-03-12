71759a2b9b96d74e3190635e4fa2100229c48a67
Tryo Asnafi <tryoasnafi@gmail.com>
2022-03-12 14:06:49 +0700

    update: Percobaan String (Array of character)

--- src/main/kotlin/17_StringDataType.kt
+++ src/main/kotlin/17_StringDataType.kt
@@ -6,10 +6,23 @@ fun main() {
     for(chr in myName){
         print(chr)
     }
-    print('\n')
+    println()
+
+    // Menggabungkan tipe data lain ke dalam String
+    val s = "abc" + 1
+    println(s + "def")
+
+    // Multi-baris String menggunakan 3x double quote (""" """)
+    var text = """
+        for (c in "foo")
+        print(c)
+    """.trimIndent() // Menghapus indentasi (whitespace) yang tidak diperlukan
+    println(text)
 
     /* Output:
     Tryo Asnafi
-
-     */
+    abc1def
+    for (c in "foo")
+    print(c)
+    */
 }
