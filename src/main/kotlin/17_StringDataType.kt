c0c5ad9fe51dcde5b684fbce9b49988ec7eea80f
Tryo Asnafi <tryoasnafi@gmail.com>
2022-03-12 14:01:01 +0700

    Percobaan String (Array of character)

---
+++ src/main/kotlin/17_StringDataType.kt
@@ -0,0 +1,15 @@
+fun main() {
+    // String merupakan kumpulan character
+    val myName: String = "Tryo Asnafi"
+
+    // Lakukan perulangan setiap character pada String
+    for(chr in myName){
+        print(chr)
+    }
+    print('\n')
+
+    /* Output:
+    Tryo Asnafi
+
+     */
+}
